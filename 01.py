63182b9b11159b9310481af10be74697d698438f
nikolai-neustroev <38642966+nikolai-neustroev@users.noreply.github.com>
2019-05-30 07:41:30 +0900

    Adding first two exercises

---
+++ 01.py
@@ -0,0 +1,2 @@
+print("What is your name?")
+print("Hello, " + input() + ", nice to meet you!")
---
+++ 02.py
@@ -0,0 +1,10 @@
+def input_len():
+    s = input()
+    if len(s) == 0:
+        print("Please enter something")
+        input_len()
+    else:
+        print(s + " has " + str(len(s)) + " characters.")
+
+print("What is the input string?")
+input_len()
