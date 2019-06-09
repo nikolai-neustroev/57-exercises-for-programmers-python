f1d7808468b58a50fffdefefde4544f71d743352
Nikolai Neustroev <38642966+nikolai-neustroev@users.noreply.github.com>
2019-06-09 21:57:47 +0900

    Update 12.py
    Small changes

--- 12.py
+++ 12.py
@@ -10,9 +10,8 @@ def input_amount():
         return input_amount()
         
 def calc_simple_interest(principal, interest, years):
-	end_amount = principal * (1.0 + interest * years)
-	return end_amount
-
+    end_amount = principal * (1.0 + interest * years)
+    return end_amount
 
 print("Enter the principal:")
 principal = input_amount()
@@ -24,6 +23,5 @@ print("Enter the number of years:")
 years = input_amount()
 
 for i in range(int(years)+1):
-	end_amount = round(calc_simple_interest(principal, interest, i), 2)
-	print(f"After {i} years at {interest * 100.0}%, the interest will be worth ${end_amount}.")
-
+    end_amount = round(calc_simple_interest(principal, interest, i), 2)
+    print(f"After {i} years at {interest * 100.0}%, the interest will be worth ${end_amount}.")
