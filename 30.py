cfa97bc4758f34069bc35a72c897fc8c25eef184
nikolai-neustroev <38642966+nikolai-neustroev@users.noreply.github.com>
2019-06-25 06:19:50 +0900

    Changing exercise 30

--- 30.py
+++ 30.py
@@ -1,6 +1,19 @@
 # 30.
 # Multiplication Table
 
-for i in range(13):
-    for j in range(13):
-        print(f"{i} x {j} = {i*j}")
+
+def table_generator():
+    for i in range(13):
+        for j in range(13):
+            if (i*j) > 0:
+                yield i*j
+
+
+for ind, el in enumerate(table_generator()):
+    if len(str(el)) == 1:
+        el = "  " + str(el)
+    elif len(str(el)) == 2:
+        el = " " + str(el)
+    else:
+        el = str(el)
+    print(el, end=" ") if ((ind+1) % 12) != 0 else print(el, end="\n")
